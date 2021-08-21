afded042d4a2f9ac226331790971bf3d6c8a7c4b
sxmeer <sameeryadav2421@gmail.com>
2021-08-21 13:12:54 +0530

    testing prop types

--- Utils/index.js
+++ Utils/index.js
@@ -1,4 +1,11 @@
+import checkPropTypes from 'check-prop-types';
+
 export const findByDataAttr = (component, dataAttr) => {
   const wrapper = component.find(`[data-test='${dataAttr}']`);
   return wrapper;
+}
+
+export const checkProps = (component, expectedProps) => {
+  const propsError = checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
+  return propsError;
 }
--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import Header from "./component/header";
 import './app.scss';
 import Headline from './component/headline';
 
+const tempArr = [{
+  fName: 'John',
+  lName: 'Doe',
+  email: 'johndoe@test.com',
+  age: 24,
+  onlineStatus: true
+}];
+
 function App() {
   return (
     <div className="App">
       <Header />
       <section className="main">
         <Headline
+          tempArr={tempArr}
           header="Posts"
           desc="Click the button to render posts" />
       </section>
